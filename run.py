cef84fc48ad017bc9a3feec639323e6214b6703a
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-07-23 05:34:01 -0400

    Run file.

---
+++ run.py
@@ -0,0 +1,17 @@
+from routes import setup
+
+
+# --------------------------------------------------------------------------- #
+#                                                                             #
+#                               Launch                                        #
+#                                                                             #
+# --------------------------------------------------------------------------- #
+def run(app):
+    app = setup(app)
+    app.run(
+        host=app.HOST,
+        port=app.PORT,
+        debug=app.DEBUG,
+        threaded=app.THREADED
+    )
+    app.sql.close()
